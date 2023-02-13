85402fd2c9674c01cd88b8a3177adc79ba33c336
unknown <malaferre@outlook.com>
2023-02-13 10:42:18 +0300

    Task4

---
+++ OneDrive/Рабочий стол/Учеба/Seminar2/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+System.Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number % 7==0 && number % 23==0)
+{
+    System.Console.WriteLine($"Кратно:");;
+}
+else
+{
+    System.Console.WriteLine($"Не кратно:");
+}
