c30110a8f64202ffada4c974b75ebdfb8f0c77bf
unknown <malaferre@outlook.com>
2023-02-13 14:07:26 +0300

    DZ-z2

---
+++ OneDrive/Рабочий стол/Учеба/Seminar2/DZ-SEM2/zadacha2/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.Write("Введите число дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1 )
+{
+    System.Console.WriteLine("weekday");
+}
+else if (number == 2)
+{
+    System.Console.WriteLine("weekday");
+}
+else if (number == 3)
+{
+    System.Console.WriteLine("weekday");
+}
+else if (number == 4)
+{
+    System.Console.WriteLine("weekday");
+}
+else if (number == 5)
+{
+    System.Console.WriteLine("weekday");
+}
+else if (number == 6)
+{
+    System.Console.WriteLine("WEEKEND");
+}
+else if (number == 7)
+{
+    System.Console.WriteLine("WEEKEND");
+}
+else
+{
+    System.Console.WriteLine("Error, there is no such day!");
+}
