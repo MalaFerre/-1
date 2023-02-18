cf42415294c527f1dd4bbb63c643e7d6c3dcbe13
MalaFerre <malaferree@outlook.com>
2023-02-18 19:50:27 +0300

    Дом.Задача

---
+++ OneDrive/Рабочий стол/Учеба/Seminar 3/DZ-task/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+if (number!.Length < 3)
+{
+    System.Console.WriteLine("число не содержит третей цифры");
+}
+else
+{
+    System.Console.WriteLine(number[2]);
+}
