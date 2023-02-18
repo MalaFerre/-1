0c6f49b4f999e50f94b8927666e9a7816b481ea5
MalaFerre <malaferree@outlook.com>
2023-02-18 20:27:05 +0300

    Task1

---
+++ OneDrive/Рабочий стол/Учеба/Seminar 3/task1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// System.Console.Write("Введите координату X: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("\nВведите координату Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if(x >0 && y > 0)
+// {
+//     System.Console.WriteLine( "I четверть");
+// }
+// else if(x<0 && y>0)
+// {
+//     System.Console.WriteLine("II Четверть");
+// }
+// else if(x<0 && y<0)
+// {
+//     System.Console.WriteLine("III Четверть");
+// }
+// else if(x>0 && y<0)
+// {
+//     System.Console.WriteLine("IV Четверть");
+// }
+// else
+// {
+//     System.Console.WriteLine("Коордитаты на осях");
+// }
+
+int[] coords = new int[2];
+System.Console.Write("Введите координату X: ");
+coords[0] = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("\nВведите координату Y: ");
+coords[1] = Convert.ToInt32(Console.ReadLine());
+
+if(coords[0]>0 && coords[1]>0)
+{
+    System.Console.WriteLine( "I четверть");
+}
+else if(coords[0]<0 && coords[1]>0)
+{
+    System.Console.WriteLine("II Четверть");
+}
+else if(coords[0]<0 && coords[1]<0)
+{
+    System.Console.WriteLine("III Четверть");
+}
+else if(coords[0]>0 && coords[1]<0)
+{
+    System.Console.WriteLine("IV Четверть");
+}
+else
+{
+    System.Console.WriteLine("Коордитаты на осях");
+}
