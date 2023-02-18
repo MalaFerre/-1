5ee557a3d9ea3a6ba4536717a01b7c6d24434968
MalaFerre <malaferree@outlook.com>
2023-02-18 21:19:44 +0300

    task2-2

--- OneDrive/Рабочий стол/Учеба/Seminar 3/task2/Program.cs
+++ OneDrive/Рабочий стол/Учеба/Seminar 3/task2/Program.cs
@@ -3,23 +3,37 @@
 
 System.Console.WriteLine("Введите номер четверти: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number == 1 )
+string[] strArray = {"x >0 и y>0", "x<0 и y>0", "x<0 и y<0", "x>0 и y<0", "Четверти не существует!"};
+if(number > 0 && number <5 )
 {
-    System.Console.WriteLine("x >0 и y>0");
-}
-else if (number == 2)
-{
-    System.Console.WriteLine("x<0 и y>0");
-}
-else if (number == 3)
-{
-    System.Console.WriteLine("x<0 и y<0");
-}
-else if (number == 4)
-{
-    System.Console.WriteLine("x>0 и y<0");
+    System.Console.WriteLine(strArray[number-1]);
 }
 else
 {
     System.Console.WriteLine("Четверти не существует!");
 }
+
+
+
+// System.Console.WriteLine("Введите номер четверти: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number == 1 )
+// {
+//     System.Console.WriteLine("x >0 и y>0");
+// }
+// else if (number == 2)
+// {
+//     System.Console.WriteLine("x<0 и y>0");
+// }
+// else if (number == 3)
+// {
+//     System.Console.WriteLine("x<0 и y<0");
+// }
+// else if (number == 4)
+// {
+//     System.Console.WriteLine("x>0 и y<0");
+// }
+// else
+// {
+//     System.Console.WriteLine("Четверти не существует!");
+// }
